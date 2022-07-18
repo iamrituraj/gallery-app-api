8bddf41f06610f5c2005d6c0e25377403006c710
Rituraj Rajput <71482941+iamrituraj@users.noreply.github.com>
2022-07-18 10:27:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+const Datastore = require("nedb");
+
+
+app.use(express.json());
+
+// app.use(
+//   express.json({
+//     limit: "5mb",
+//   })
+// );
+
+const db = new Datastore("../gallery-app/db/data.db");
+
+db.loadDatabase();
+
+app.get("/api/images", async (req, res) => {
+  console.log("hi");
+  try {
+    db.loadDatabase();
+
+    db.find({}, (err, data) => {
+      res.status(200).json(data);
+    })
+  }
+
+  catch (err) {
+
+    return res.status(500).json(err);
+  }
+});
+
+app.get("/api/images/:id", async (req, res) => {
+  try {
+    db.loadDatabase();
+    db.find({ _id: req.params.id }, (err, data) => {
+      res.status(200).json(data);
+    });
+  } catch (err) {
+
+    return res.status(500).json(err);
+  }
+});
+
+
+app.post("/api", async (req, res) => {
+  try {
+    db.loadDatabase();
+    const data = req.body;
+
+    db.insert(data, (err) => {
+      res.status(200).json("ok");
+    });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+
+app.put("/api/:id/edit", async (req, res) => {
+  try {
+    db.loadDatabase();
+    const data = req.body;
+       console.log(data);
+    db.update(
+      { _id: req.params.id },
+      { ImgName:req.body.ImgName , ImgUrl: req.body.ImgUrl, ImgDetails: req.body.ImgDetails },
+      { multi: false },
+      (err) => {
+        res.status(200).json("ok");
+      }
+    );
+
+    db.loadDatabase();
+
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+app.delete("/api/delete/:id", async (req, res) => {
+  try {
+    db.loadDatabase();
+    const data = req.body;
+
+    db.remove({ _id: req.params.id }, (err) => {
+      res.status(200).json("ok");
+    });
+    db.loadDatabase();
+
+  } catch (err) {
+
+    return res.status(500).json(err);
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server is running");
+});
